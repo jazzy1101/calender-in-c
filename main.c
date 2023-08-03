ea441227b3c08efb3ee9b639638198ba8ed98e6d
darambae <64197421+jazzy1101@users.noreply.github.com>
2023-08-03 16:59:20 +0200

    inserted month and year input function

--- main.c
+++ main.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+enum { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+
 int main()
 {
-	printf("------------ \n");
+	int month, year;
+	printf("Type a month and year of the calender you wish to check using number key");
+	scanf("%d,%d", month, year);
+	switch (month)
+	{
+	  case 1: 
+		month = "JAN";
+		break;
+	  case 2:
+		month = "FEB";
+		break;
+	  case 3:
+		month = "MAR";
+		break;
+	  case 4:
+		month = "APR";
+		break;
+	  case 5:
+		month = "MAY";
+		break;
+	  case 6:
+		month = "JUN";
+		break;
+	  case 7:
+		month = "JUL";
+		break;
+	  case 8:
+		month = "AUG";
+		break;
+	  case 9:
+		month = "SEP";
+		break;
+	  case 10:
+		month = "OCT";
+		break;
+	  case 11:
+		month = "NOV";
+		break;
+	  case 12:
+		month = "DEC";
+		break;
+	  default:
+		printf("Please enter a number from 1 to 12");
+	}	
+	printf("----------------- \n");
+	printf("  %d  %d  \n", month, year);
+	printf("----------------- \n");
+	
+	
 	return 0;
 }
