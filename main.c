6449403e2d4b2dbf8c72aa3c178e87ecfe15fb75
darambae <64197421+jazzy1101@users.noreply.github.com>
2023-08-04 17:00:39 +0200

    Printing the calender head accordingly

--- main.c
+++ main.c
@@ -1,56 +1,17 @@
 #include <stdio.h>
-enum { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+#include <string.h>
 
+const char* month[12] = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 int main()
 {
-	int month, year;
-	printf("Type a month and year of the calender you wish to check using number key");
-	scanf("%d,%d", month, year);
-	switch (month)
-	{
-	  case 1: 
-		month = "JAN";
-		break;
-	  case 2:
-		month = "FEB";
-		break;
-	  case 3:
-		month = "MAR";
-		break;
-	  case 4:
-		month = "APR";
-		break;
-	  case 5:
-		month = "MAY";
-		break;
-	  case 6:
-		month = "JUN";
-		break;
-	  case 7:
-		month = "JUL";
-		break;
-	  case 8:
-		month = "AUG";
-		break;
-	  case 9:
-		month = "SEP";
-		break;
-	  case 10:
-		month = "OCT";
-		break;
-	  case 11:
-		month = "NOV";
-		break;
-	  case 12:
-		month = "DEC";
-		break;
-	  default:
-		printf("Please enter a number from 1 to 12");
-	}	
+	int input, year;
+	printf("Type a month and year of the calender you wish to check using number key\n");
+	scanf("%d ,%d", &input, &year);
+			
 	printf("----------------- \n");
-	printf("  %d  %d  \n", month, year);
+	printf("    %s  %d   \n", month[input - 1], year);
 	printf("----------------- \n");
-	
+	printf("  M T W T F S S	 \n");
 	
 	return 0;
 }
